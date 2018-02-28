7cfa7e8d3cce4d19d9adb029ed728d21872cc10f
Arthur Sultan <arthur.sultan.info@gmail.com>
2018-02-28 21:45:33 +0100

    Updated ending date

--- config/presaleConfig.js
+++ config/presaleConfig.js
@@ -3,7 +3,7 @@ module.exports = Object.freeze({
     PRESALE_OWNER_NUMBER: 0,
     PRESALE_RATE: 500,
     PRESALE_START_TIME: 1521932400, // 25/03/2018 00:00:00
-    PRESALE_END_TIME: 1524693600,// 26/04/2018 00:00:00
+    PRESALE_END_TIME: 1524520800,// 24/04/2018 00:00:00
     PRESALE_INVESTOR_NUMBER: 1  // For TestLabStartPresale
                               // TODO(optional): rename as PRESALE_INVESTOR_INDEX
 });
