60fdd18a0d60a43d5984d480bef7a8e72b4c1cfb
steroides <arthur.sultan.info@gmail.com>
2018-03-11 20:44:30 +0100

    Update LabCoin to derive from StandardToken

--- contracts/LabCoin.sol
+++ contracts/LabCoin.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.19;
 
 import "zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract LabCoin is BurnableToken {
+contract LabCoin is StandardToken, BurnableToken {
     string public constant name = "LabCoin";
     string public constant symbol = "LAB";
     uint8 public constant decimals = 18;
